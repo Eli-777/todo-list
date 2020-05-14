d080a298a2fc49a114f1d588e88417b9b717e601
mrsandwitch <mrsandwitch@gmail.com>
2020-05-14 13:29:04 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
 
-//接住表單，並送往資料庫
+//接住表單，並送往資料庫 ＝ create 功能
 app.post('/todos', (req, res) => {
   const name = req.body.name //從 req.body 拿出表單的 name 資料
   return Todo.create({ name })  //呼叫Todo 物件直接新增資料存入資料庫
@@ -58,6 +58,28 @@ app.get('/todos/:id', (req, res) => {
   return Todo.findById(id) //取出 Todo model 裡的 id 資料
     .lean() //把 Mongoose 的 Model 物件轉換成乾淨的JavaScript資料陣列
     .then((todo) => res.render('detail', { todo })) //將資料傳給 detail 樣板
+    .catch(error => console.log(error))
+})
+
+//新增修改頁面
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id) //取出 Todo model 裡的 id 資料
+    .lean() //把 Mongoose 的 Model 物件轉換成乾淨的JavaScript資料陣列
+    .then((todo) => res.render('edit', { todo })) //將資料傳給 edit 樣板
+    .catch(error => console.log(error))
+})
+
+//接住修改頁面的表單，把資料庫的資料改成表單資料 = update 功能
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name  //把資料庫資料 name 改成收到的表單資料
+      return todo.save() //儲存此筆資料
+    })
+    .then(()=> res.redirect(`/todos/${id}`)) //回到詳細頁面
     .catch(error => console.log(error))
 })
 
