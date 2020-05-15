fcf91e4411ae98b1bb18db6297e2b4c0203a4175
Eli <jo458405@hotmail.com>
2020-05-15 17:19:12 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-
 // 引用路由器
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 //設定樣版引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -37,7 +21,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 // 將 request 導入路由器
 app.use(routes)
-
 
 
 // 設定 port 3000
