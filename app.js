dbfbdea66d13b4b9d8d71a862883f2888a10e32a
Eli <jo458405@hotmail.com>
2020-05-15 14:31:07 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() //取出 Todo model 裡的所有資料
     .lean() //把 Mongoose 的 Model 物件轉換成乾淨的JavaScript資料陣列
+    .sort({ _id: 'asc'}) //asc = 升冪  desc＝ 降冪
     .then(todos => res.render('index', { todos })) //將資料傳給 index 樣板
     .catch(error => console.error(error)) //錯誤處理
 })
